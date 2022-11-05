9c04d10fe60b08405028cf249a6e50c0a1bfb8b8
seba-1511 <seba-1511@hotmail.com>
2022-11-05 07:18:44 +0000

    Update tensorboard command.

--- run_tensorboard.sh
+++ run_tensorboard.sh
@@ -1,1 +1,3 @@
+# tensorboard --logdir gs://melodi-bucket0/melodi_evaluation/ --load_fast false --host 0.0.0.0
 tensorboard --logdir gs://melodi-bucket0/models/ --load_fast false --host 0.0.0.0
+# tensorboard --logdir gs://melodi-bucket0/melodi_training/ --load_fast false --host 0.0.0.0
