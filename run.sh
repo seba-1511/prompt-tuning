8736986d1839d976f0402326290d09e2be34052f
seba-1511 <seba-1511@hotmail.com>
2022-11-08 17:43:33 +0000

    Update path.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 MODEL_DIR='./models' \
 T5X_DIR='/home/seba-1511/t5x/' \
 TFDS_DATA_DIR='/home/seba-1511/tfds_data' \
-./prompt_tuning/scripts/sst2-demo.sh gs://melodi-bucket0/melodi_evaluation/task=squad/model=xl_lm100_spot/method=melodi-h32-lr-1.0-matched /home/seba-1511/tfds_data
+./prompt_tuning/scripts/sst2-demo.sh gs://melodi-bucket0/melodi_evaluation/task=mnli/model=xl_lm100_spot/method=adafactor-lr-0.3 /home/seba-1511/tfds_data
