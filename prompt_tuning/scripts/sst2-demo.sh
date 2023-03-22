ace3385618e8d3bf55ea7b8ad87cf06afa199a77
seba-1511 <seba-1511@hotmail.com>
2023-03-22 18:09:54 +0000

    Update for precomputed_opt command.

--- prompt_tuning/scripts/sst2-demo.sh
+++ prompt_tuning/scripts/sst2-demo.sh
@@ -25,15 +25,15 @@ python3 -m t5x.train \
   --gin_file="prompt_tuning/configs/models/t5_1_1_xl_prompt.gin" \
   --gin_file="prompt_tuning/configs/runs/prompt_finetune.gin" \
   --gin_file="prompt_tuning/configs/melodi/optax_optimizer.gin" \
-  --gin_file="../melodi/experimental/gins/tasks/mnli.gin" \
+  --gin_file="../melodi/experimental/gins/tasks/flan.gin" \
   --gin_file="../melodi/experimental/gins/methods/prompt_init/spot_c4.gin" \
-  --gin.FLAN_TASK="'hellaswag_type_0'" \
+  --gin.FLAN_TASK="'mnli_mismatched_type_0'" \
   --gin.MODEL_DIR="'${MODEL_DIR}'" \
   --gin.INITIAL_CHECKPOINT_PATH="'${PRETRAINED_MODEL}'" \
   --gin.TRAIN_STEPS="1_103_000" \
   --gin.EVAL_PERIOD=50 \
   --gin.DROPOUT_RATE=0.0 \
-  --gin.OPTAX_LEARNING_RATE=0.3 \
+  --gin.OPTAX_LEARNING_RATE=1.0 \
   --gin.OPTAX_MOMENTUM=0.0 \
   --gin.OPTAX_MELODI_PATH='"gs://melodi-bucket0/melodi_training/xl-newhyper/task=flan10star_nodropout_cut1500_flan15_eval/model=small_sequence/horizon=4/memory=128/bsz=256/lr=5e-5/1676425165/inference-adafactor-3k-1677209557/train_updates"' \
   --gin.OPTAX_MELODI_MEMORY=128 \
