d0ac095dd840502dc2ea00c60b693915dbbbaede
seba-1511 <seba-1511@hotmail.com>
2023-06-20 22:07:32 +0000

    Update tensorboard commands.

--- run_tensorboard.sh
+++ run_tensorboard.sh
@@ -1,4 +1,5 @@
-tensorboard --logdir gs://melodi-bucket0/melodi_evaluation/ --load_fast false --host 0.0.0.0
+tensorboard --logdir gs://melodi-bucket0/melodi_evaluation/task=mnli_mismatched_type_0/ --load_fast false --host 0.0.0.0 --port=60066
+# tensorboard --logdir gs://melodi-bucket0/melodi_evaluation/ --load_fast false --host 0.0.0.0
 # tensorboard --logdir gs://melodi-bucket0/melodi_evaluation/ --load_fast false --samples_per_plugin scalars=999999999 --host 0.0.0.0
 # tensorboard --logdir gs://melodi-bucket0/models/ --load_fast false --host 0.0.0.0
 # tensorboard --logdir gs://melodi-bucket0/melodi_training/ --load_fast false --host 0.0.0.0
